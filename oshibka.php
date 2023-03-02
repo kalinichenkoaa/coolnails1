1156b5a8c4a69ec3e154dc6db75d6e653cea02b6
kalinichenkoaa <126446435+kalinichenkoaa@users.noreply.github.com>
2023-03-02 21:51:39 +0500

    Add files via upload

---
+++ oshibka.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav> 
+                <ul class="ul_menu_left">
+                    <li><a href="index.html">Главная</a></li>
+                    <li class="li_left"><a href="index.html#news">Новости</a></li>
+                </ul>
+                <div class="logo">
+                    <a href="index.html"><img src="images/logo1.png" alt="логотип"></a>
+                </div>
+                <ul class="ul_menu_right">
+                    <li class="li_right"><a href="index.html#services">Услуги</a></li>
+                    <li><a href="company.html">О нас</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    </header>
+    <main>
+        <div class="container">
+        <div class="stroka_2">
+            <h1>404</h1>
+            <h2>Просим прощения, но страница, которую вы искали - не нашлась!</h2>
+        </div>   
+        <div class="stroka_3">
+            <p>Возможно это произошло из-за опечатки при вводе адреса страницы или потому что
+                страница усторела и была удалина. В любом случае, мы очень сожалеем и предлагаем воспользоваться
+                формой поиска, надеемся, страница найдётся.</p>
+            <div class="knopki1">
+                <div class="naglavnuy"><button><a href="index.html">Перейти на главную</a></button></div>
+            </div>
+        </div></div>
+    </main>
+    <footer>
+        <div class="container">
+            <div class="footer">
+                <img src="images/logo1.png" alt="логотип" class="logo_img">
+                <ul class="ul_menu_footer">
+                    <li><a href="#news">Новости</a></li>
+                    <li><a href="#services">Услуги</a></li>
+                    <li><a href="#about_company">О нас</a></li>
+                    <li><a href="contacts.html">Контакты</a></li>
+                </ul>
+                <div class="social_network">
+                    <p>Следите за нами</p>
+                    <a href="https://vk.com/oduvanchik_16"><img src="images/VK.png" alt="vk"></a>
+                </div>
+            </div>    
+        </div>
+    </footer>
+</body>
+</html>
